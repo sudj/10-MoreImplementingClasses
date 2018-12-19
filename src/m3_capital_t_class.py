8b3f093689712db09b67f42077fd4e135c402fc6
sudj <sudj@rose-hulman.edu>
2018-12-19 12:23:02 -0500

    DONE

--- src/m3_capital_t_class.py
+++ src/m3_capital_t_class.py
@@ -22,8 +22,8 @@ def main():
 
     # run_test_simple_t()
     # run_test_set_colors()
-    # run_test_move_by()
-    run_test_clone()
+    run_test_move_by()
+    # run_test_clone()
 
 
 def run_test_simple_t():
